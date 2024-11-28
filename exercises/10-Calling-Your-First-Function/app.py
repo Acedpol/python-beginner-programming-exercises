7a9c910517efe36d15f5694280bb89f99ee79ffc
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-11-28 10:21:35 +0000

    exercises 1-12

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -4,3 +4,12 @@ def is_odd(my_number):
 
 def my_main_code():
     # ✅ ↓ Your code here ↓ ✅
+	f = is_odd
+	print(f(45345))
+	# num = 45345
+	# if is_odd(num):
+	# 	print("yes, the number {} is odd".format(num))
+	# else:
+	# 	print("nope, the number {} isn't odd, it is pair".format(num))
+		
+my_main_code()
--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,10 @@
 import random
 
 # ✅↓ Write your code here ↓✅
+# r1 = random.randint(0, 10)
+# print("Random number between 0 and 10 is %s" % (r1))
+
+def generate_random():
+    return random.randrange(9)
+
+print(generate_random())
