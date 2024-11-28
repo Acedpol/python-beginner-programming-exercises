25023152e5e588feecb91c1200c20a7e85a32f09
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-11-28 14:58:30 +0000

    exercises 16-19

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,17 @@
 # ✅↓ Write your code here ↓✅
+def estribillo(n):
+    lyrics = ""
+    for i in range(1,n+1):
+        lyrics += "let it be,\n"
+    return lyrics
+
+def sing():
+    lyrics = ""
+    lyrics += estribillo(4)
+    lyrics += "there will be an answer,\n"
+    lyrics += estribillo(5)
+    lyrics += "whisper words of wisdom, let it be"
+    return lyrics
+
+print(sing())
 
