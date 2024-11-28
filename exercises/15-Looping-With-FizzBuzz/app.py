f54370f3ece1523a8f834fadbfb2ceca92a8eeaa
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-11-28 10:44:44 +0000

    exercises 12-15

--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,17 @@
 def fizz_buzz():
     # ✅↓ Write your code here ↓✅
+    for i in range(100):
+        n = i + 1
+        if(n % 3 == 0 and n % 5 == 0):
+            print("FizzBuzz")
+        elif(n % 3 == 0):
+            print("Fizz")
+        elif(n % 5 == 0):
+            print("Buzz")
+        else:
+            print(n)
+    return i
+        
 
 # ❌↓ DON'T CHANGE THE CODE BELOW ↓❌
 fizz_buzz()
